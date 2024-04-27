21cb190dec696187af3df221aa41a100581988b9
Barruku <141589210+Panagiwthsbarruku@users.noreply.github.com>
2024-04-27 10:59:15 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function showAlert() {
+    alert("Η παραγγελία στάλθηκε επιτυχώς στις κεντρικές αποθήκες");
+}
+function paragelia() {
+    window.location.href = "http://127.0.0.1:3000/paragelia.html";
+}
+function paralabi() {
+    window.location.href = "http://127.0.0.1:3000/paralabi.html";
+}
+function Reset(){
+    window.location.href="http://127.0.0.1:3000/index.html";
+}
+ function send(){
+    alert("Επιτυχής αποστολή");
+}
